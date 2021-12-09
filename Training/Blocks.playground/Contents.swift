9d2c945b95d49fc9675af17690538c2ae0ea992d
Carlos A Alba <ing.alfredo.alba@gmail.com>
2021-12-08 17:44:41 -0800

    MVC Project
    Small Project in MVC

--- Training/Blocks.playground/Contents.swift
+++ Training/Blocks.playground/Contents.swift
@@ -1,15 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
-
 var thing = "cars"
 
 let closer = {
-    print("Hola \(thing)")
+    print("Hi \(thing)")
 }
 
-let closer2 = { [thing] in
-    print("Hola \(thing)")
+let closer2 = { [thing] in // TODO: check closer "in"
+    print("Hi \(thing)")
 }
 
 thing = "airplanes"
@@ -17,3 +15,7 @@ thing = "airplanes"
 closer()
 
 closer2()
+
+
+//     * They are two pointers wide, where the first pointer is a function pointer pointing to the code that implements the closure, and the second pointer optionally points to a reference counted object containing the closed-over state.
+//  So, yes, the capture list is heap allocated, and it is reference counted.
